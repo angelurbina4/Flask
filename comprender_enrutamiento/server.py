905fb8d88924f3c8950497c5d8ccfdd4913906a2
Angelurbina4 <angelurbina44@gmail.com>
2022-03-03 18:32:49 -0300

    Comprender enrutamiento

---
+++ comprender_enrutamiento/server.py
@@ -0,0 +1,37 @@
+from distutils.log import debug
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def hola_mundo():
+    return "<h1>¡Hola Mundo!</h1>"
+
+@app.route('/dojo')
+def dojo():
+    return "<h2>¡Dojo!</h2>"
+
+@app.route('/say/<name>')
+def hola_name(name):
+    str(name)
+    return f"Hola {name}"
+
+@app.route('/repeat/<int:num>/<name>')
+def repeat_name(num, name):
+    int(num)
+    str(name)
+    output = ''
+    for i in range(0, num):
+        output += '<p>'+name+'</p>'
+    return output
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+#pip3 install pipenv
+#pipenv install flask
+#python3 -m pipenv <command to use> SOLO SI RECIBIMOS ERROR
+#pipenv shell
+#python3 server.py
