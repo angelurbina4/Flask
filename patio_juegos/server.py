fa9a2df91581def8c6c3ad40dba1e3980bc0cb5a
Angelurbina4 <angelurbina44@gmail.com>
2022-03-04 10:53:26 -0300

    patio de juegos

--- patio_juegos/server.py
+++ patio_juegos/server.py
@@ -8,15 +8,15 @@ def home():
 
 @app.route('/play')
 def play():
-    return render_template("index.html", color="lightblue", num=3)
+    return render_template("index.html", color ='lightblue', num = 3)
 
 @app.route('/play/<int:num>')
 def play2(num):
-    return render_template("index.html", color ="lightblue", num=num)
+    return render_template("index.html", color ='lightblue', num = num)
 
 @app.route('/play/<int:num>/<string:color>')
-def play(num, color):
-    return render_template("index.html", color =color, num=num)
+def play3(num, color):
+    return render_template("index.html", color = color, num = num)
 
 if __name__ == "__main__":
     app.run(debug=True)
